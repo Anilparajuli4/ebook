57755f1d3d553f784f2af7cd1158cbb1bc66ace9
Anil Parajuli <anilparajuli998@gmail.com>
2024-04-11 22:29:21 +0545

    add register user validation

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 
-import { NextFunction, Request, Response } from 'express'
+
 import app from './src/app'
 import { config } from './src/config/config'
 import connectDB from './src/config/db'
-import createHttpError, { HttpError } from 'http-errors'
+
 
 
 const startServer = async() =>{
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 import express, { NextFunction, Request, Response } from 'express'
 import createHttpError from 'http-errors'
+import bodyParser from 'body-parser';
 import { globalErrorHandler } from './middleware/globalErrorHandler'
+import useRouter from './user/userRouter'
 
 const app = express()
+app.use(express.json())
+// app.use(bodyParser.json())
 
 app.get('/', (req:Request, res:Response, next:NextFunction)=>{
-    const error = createHttpError(400, "something went wrong") 
-    throw error
-    res.json({message: "welcome to the"})
+    try {
+        // const error = createHttpError(400, "something went wrong");
+        // throw error; // You might want to handle this differently based on your logic.
+        res.json({ message: "welcome to the" });
+    } catch (err) {
+        next(err); // Pass the error to the global error handler
+    }
 })
 
 
-
+app.use('/api/users', useRouter)
 app.use(globalErrorHandler)
 
 export default app;

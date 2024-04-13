c6704756ae665239248ff94168a3727c2b671a08
Anil Parajuli <anilparajuli998@gmail.com>
2024-04-13 15:11:19 +0545

    upload files to cloudiary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,52 @@
+import path from 'node:path'
 import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
+import { log } from 'node:util';
+import createHttpError from 'http-errors';
 
 const createBook = async(req:Request, res:Response, next:NextFunction)=>{
 
+
+      const files = req.files as {[filename:string]: Express.Multer.File[]}
+
+
+   
+
+     const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1)
+     const fileName = files.coverImage[0].filename
+     const filePath = path.resolve(__dirname, '../../public/data/uploads', fileName)
+     
+     try {
+      const uploadResult = await cloudinary.uploader.upload(filePath, {
+         filename_override:fileName,
+         folder: 'book-covers',
+         format: coverImageMimeType
+      })
+         
+     } catch (error) {
+       console.log(error);
+       next(createHttpError(500, 'while uploading file'))
+         
+
+     }
+
+ 
+
+     const bookFileName = files.file[0].filename
+     const bookFilepath = path.resolve(__dirname, '../../public/data/uploads', bookFileName) 
+     const bookFileUploadResult = await cloudinary.uploader.upload(bookFilepath, {
+      resource_type: 'raw',
+      filename_override: bookFileName,
+      folder:'book-pdfs',
+      format: 'pdf'
+     })
+     
+    console.log("bookFileUploadResult",bookFileUploadResult);
+
+
+    
+    
+    res.json({})
 }
 
 export{createBook}
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,10 @@ const _config ={
    port:process.env.PORT,
    dataabaseUrl: process.env.MONGO_CONNECTION,
    env:process.env.NODE_ENV,
-   jwtsecret:process.env.JWT_SECRET
+   jwtsecret:process.env.JWT_SECRET,
+   cloudinaryCloud:process.env.CLOUDINARY_CLOUD,
+   cloudinaryApi:process.env.CLOUDINARY_API,
+   cloudinarySecret:process.env.CLOUDINARY_SECRET
 }
 
 
